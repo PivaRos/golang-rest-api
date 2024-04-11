e4c837ad94e7c7f2ccc5712d986318794eaf412e
Daniel Gurbin <daniel.g@msapps.mobi>
2024-04-11 23:09:39 +0300

    fix struct

--- src/services/user.go
+++ src/services/user.go
@@ -7,19 +7,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// !change the fields
 type UserCreate struct {
-	user     structs.PrivateUser `json:"user"`
-	password string              `json:"password"`
+	structs.PrivateUser
+	password string `json:"password"`
 }
 
-// !fix function and logic
 func CreateUser(user UserCreate) (*mongo.InsertOneResult, error) {
 	env := structs.AppMain.Env
 	db := structs.AppMain.MongoClient.Database(env.Db)
 	result, err := db.Collection("users").InsertOne(context.TODO(), user)
 	return result, err
-
 }
 
 func DeleteUser() {
